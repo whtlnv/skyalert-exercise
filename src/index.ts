f25efb787042c57dd05edde5c1bebb3679985228
Carlos Martínez <ced.mendoza@gmail.com>
2022-02-08 11:49:07 -0600

    spec: Add tests for `maskify` function (only signature is implemented)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+export const maskify = (creditCardNumber: String): String => {
+  return creditCardNumber;
 };

eced8e3aa77737785e969f4e18df48db394eace6
Carlos Martínez <ced.mendoza@gmail.com>
2022-02-08 12:03:36 -0600

    feat: Write implementation for test, Closes #1

--- test/exercise1.test.ts
+++ test/exercise1.test.ts
@@ -7,10 +7,11 @@ describe('maskify', () => {
 
   it('mask all digits except for first and last four digits for string of 8 and 7 charcters', () => {
     expect(maskify('12345678')).toEqual('1xxx5678');
-    expect(maskify('1234567')).toEqual('1xxxx67');
+    expect(maskify('1234567')).toEqual('1xx4567');
   });
 
   it('does not mask any digits if string is less than 7 characters', () => {
     expect(maskify('123456')).toEqual('123456');
+    expect(maskify('1')).toEqual('1');
   });
 });

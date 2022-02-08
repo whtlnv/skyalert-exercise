cbdbc50c0fbb446e8ac832ea19d0ff05f14df924
Carlos Martínez <ced.mendoza@gmail.com>
2022-02-08 13:08:18 -0600

    fix: Formatting issues

--- src/index.ts
+++ src/index.ts
@@ -21,17 +21,25 @@ export const maskify = (creditCardNumber: String): String => {
 
 export const numberToOrdinal = (n: number): string => {
   if (n < 0) {
-    throw new Error("ordinal numbers must be positive");
+    throw new Error('ordinal numbers must be positive');
   }
 
   const nStr = String(n);
-  const exceptions: { [key: string]: string } = {'0': '0', '11': '11th', '12': '12th', '13': '13th'};
+  const exceptions: { [key: string]: string } = {
+    '0': '0',
+    '11': '11th',
+    '12': '12th',
+    '13': '13th',
+  };
   if (nStr in exceptions) {
     return exceptions[nStr];
   }
 
-  const suffixes: { [key: string]: string } = {'1': 'st', '2': 'nd', '3': 'rd'};
+  const suffixes: { [key: string]: string } = {
+    '1': 'st',
+    '2': 'nd',
+    '3': 'rd',
+  };
   const lastDigit: string = nStr.substring(nStr.length - 1, nStr.length);
-
   return nStr + (lastDigit in suffixes ? suffixes[lastDigit] : 'th');
 };
